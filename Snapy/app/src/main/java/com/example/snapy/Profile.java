c41258f44ccc74bd3cee7a8ccb8fe209ea3bd7e8
Ankit Kirar <70141759+aankit001@users.noreply.github.com>
2020-10-02 10:54:41 +0530

    profile
    profie activity set

--- Snapy/app/src/main/java/com/example/snapy/Profile.java
+++ Snapy/app/src/main/java/com/example/snapy/Profile.java
@@ -2,13 +2,31 @@ package com.example.snapy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.time.Instant;
 
 public class Profile extends AppCompatActivity {
-
+    ImageView imageView;
+    TextView profileName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        imageView=findViewById(R.id.arrowback);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),WelcomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        profileName=findViewById(R.id.profileName);
+        
     }
 }
